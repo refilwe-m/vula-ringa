a9ae5e9e525c6be8e0796c4f3440c7aa5c161f67
refilwe-m <refilwe.dev@gmail.com>
2025-04-29 10:36:02 +0200

    Local Stashes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from "react-native";
+import { useTailwindCss } from "../../hooks/useTailwind";
 
 export default function HomeScreen() {
+  const tw = useTailwindCss();
   return (
-    <View>
+    <View style={tw("bg-red-500")}>
       <Text>Test</Text>
     </View>
   );
---
+++ hooks/useTailwind.ts
@@ -0,0 +1,6 @@
+import { useTailwind } from "tailwind-rn";
+
+export const useTailwindCss = () => {
+  const tw = useTailwind();
+  return tw;
+};
